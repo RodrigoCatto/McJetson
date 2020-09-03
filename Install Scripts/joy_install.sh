518813018cc4242e63627e643045d5b0715941e0
RodrigoCatto <rodrigo.catto7@gmail.com>
2020-09-03 14:15:31 -0300

    Install Scripts

---
+++ Install Scripts/joy_install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Install Joy Pkg to read the controller
+sudo apt install ros-melodic-joy
+
+# Intall Twist Joy Pkg to transform controller data to twist msg
+sudo apt install ros-melodic-twist-joy
+
+# Run the launch file and check the commands
+
+#
